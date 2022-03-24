e2465e7a4be815856ec69e835371c545495f6406
Chmalo <chmalo.f@gmail.com>
2022-03-24 09:08:19 -0400

    feat: test basico de GifGridItem 3

--- src/tests/components/GifGridItem.test.tsx
+++ src/tests/components/GifGridItem.test.tsx
@@ -5,10 +5,30 @@ import GifGridItem from "../../components/GifGridItem"
 describe("test GifGridItem", () => {
   const title = "test title"
   const url = "http://localhost/algo.jpg"
+  const wrapper = shallow(<GifGridItem title={title} url={url} />)
 
   test("should GifGridItem correctly", () => {
-    const wrapper = shallow(<GifGridItem title={title} url={url} />)
-
     expect(wrapper).toMatchSnapshot()
+  })
+
+  test("should has paragraph with title", () => {
+    const paragraph = wrapper.find("p")
+
+    expect(paragraph.text().trim()).toBe(title)
+  })
+
+  test("should has image equal url and alt of props", () => {
+    const img = wrapper.find("img")
+    const { src, alt } = img.props()
+
+    expect(src).toBe(url)
+    expect(alt).toBe(title)
+  })
+
+  test("should has animate__fadeIn", () => {
+    const div = wrapper.find("div")
+    const className = div.prop("className")
+    // @ts-ignore
+    expect(className.includes("animate__fadeIn")).toBe(true)
   })
 })
