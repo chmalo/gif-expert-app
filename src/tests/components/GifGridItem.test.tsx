f8a5156efe5cdddc475f926faa285bc7962665cc
Chmalo <chmalo.f@gmail.com>
2022-03-24 08:38:21 -0400

    feat: test basico de GifGridItem 2

--- src/tests/components/GifGridItem.test.tsx
+++ src/tests/components/GifGridItem.test.tsx
@@ -3,8 +3,11 @@ import { shallow } from "enzyme"
 import GifGridItem from "../../components/GifGridItem"
 
 describe("test GifGridItem", () => {
+  const title = "test title"
+  const url = "http://localhost/algo.jpg"
+
   test("should GifGridItem correctly", () => {
-    const wrapper = shallow(<GifGridItem />)
+    const wrapper = shallow(<GifGridItem title={title} url={url} />)
 
     expect(wrapper).toMatchSnapshot()
   })
