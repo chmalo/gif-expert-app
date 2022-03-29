4a052b45a53de319c510aaa93cbe3acaeb317640
Chmalo <chmalo.f@gmail.com>
2022-03-29 08:41:49 -0400

    feat: testing in addcategory

--- src/helpers/getGifs.tsx
+++ src/helpers/getGifs.tsx
@@ -1,9 +1,11 @@
+import axios from "axios"
+
 export const getGifs = async (category: string) => {
   const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
   )}&limit=10&api_key=rMr9AzqzYbnJiBtEuJOKTN77v6vJN97S`
-  const resp = await fetch(url)
-  const { data } = await resp.json()
+  const resp = await axios(url)
+  const { data } = await resp.data
   const gifs = data.map((img: any) => {
     return {
       id: img.id,
---
+++ src/tests/helpers/getGifs.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest"
+import { getGifs } from "../../helpers/getGifs"
+
+describe("test with getGifs", () => {
+  test("should return then element", async () => {
+    const gifs = await getGifs("goku")
+
+    expect(gifs.length).toBe(10)
+  }, 5000)
+
+  test("should return empty array element", async () => {
+    const gifs = await getGifs("")
+
+    expect(gifs.length).toBe(0)
+  }, 5000)
+})
